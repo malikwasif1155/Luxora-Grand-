48be863f673b82a28cddd99b60d66e52e1ae72e3
malikwasif1155 <malikwasif1155@gmail.com>
2026-09-24 00:37:40 +0500

    Fix static export by using public robots file

--- app/robots.ts
+++
@@ -1,1 +0,0 @@
-import type {MetadataRoute} from 'next';export default function robots():MetadataRoute.Robots{return {rules:{userAgent:'*',allow:'/'},sitemap:'https://luxoragrand.example.com/sitemap.xml'}}
